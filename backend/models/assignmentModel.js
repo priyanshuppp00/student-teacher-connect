2a21c7714839cc60d3fd1f01ca42349d3aa9667c
Priyanshu Kumar <priyanshucoolpks@gmail.com>
2025-10-28 19:00:53 +0530

    Update

--- backend/models/assignmentModel.js
+++ backend/models/assignmentModel.js
@@ -9,7 +9,7 @@ const assignmentSchema = new mongoose.Schema(
     teacher: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     },
   },
   { timestamps: true }
